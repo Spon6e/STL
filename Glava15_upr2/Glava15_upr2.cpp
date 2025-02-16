d56191c8f02fe62f79552050ef313376b59c545d
Danil Zholobov <asusmemp10@gmail.com>
2025-02-16 04:28:47 +0300

    Сделал упражнение 2

---
+++ Glava15_upr2/Glava15_upr2.cpp
@@ -0,0 +1,33 @@
+﻿#include <string>
+#include <iostream>
+#include <algorithm>
+#include <conio.h>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    vector<string> mas;
+    string slovoSPB;
+    char ch;
+    int i = 0;
+    do {
+        cout << "Введите слово: ";
+        cin >> slovoSPB;
+        cout << "Продолжим?(y/n)" << endl;
+        cin >> ch;
+        mas.push_back(slovoSPB);
+        i++;
+    } while (ch == 'y');
+    cout << "no sort" << endl;
+    for (int j = 0; j < i; j++) {
+        cout << mas[j] << endl;
+    }
+    cout << "sort" << endl;
+    sort(mas.begin(), mas.end());
+    for (int j = 0; j < i; j++) {
+        cout << mas[j] << endl;
+    }
+    _getch();
+}
