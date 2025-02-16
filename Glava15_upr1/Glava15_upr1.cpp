6ce9967c56b1b1339abb12821334ae48f2de62a7
Danil Zholobov <asusmemp10@gmail.com>
2025-02-16 03:44:33 +0300

    Сделал упражнение 1

---
+++ Glava15_upr1/Glava15_upr1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float mas[100];
+    float chislo;
+    char ch;
+    int i = 0;
+    do {
+        cout << "Введите число: ";
+        cin >> chislo;
+        mas[i++] = chislo;
+        cout << "Продолжим?(y/n)" << endl;
+        cin >> ch;
+    } while (ch == 'y');
+    cout << "No sort" << endl;
+    for (int j = 0; j < i; j++) {
+        cout << mas[j] << endl;
+    }
+    cout << "Sort" << endl;
+    sort(mas, mas + i);
+    for (int j = 0; j < i; j++) {
+        cout << mas[j] << endl;
+    }
+    _getch();
+}
