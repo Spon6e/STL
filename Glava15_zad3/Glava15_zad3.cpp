b084d967e08db72f6c3457cbd50de20d0cd8b63a
Danil Zholobov <asusmemp10@gmail.com>
2025-02-17 19:12:18 +0300

    Сделал упражнение 3

---
+++ Glava15_zad3/Glava15_zad3.cpp
@@ -0,0 +1,27 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <conio.h>
+#include <list>
+using namespace std;
+
+int main()
+{
+    list<int> spisok;
+    list<int>::iterator iter1;
+    list<int>::iterator iter2;
+    for (int i = 0; i < 16; i++)
+        spisok.push_back(i);
+    iter1 = spisok.begin();
+    iter2 = spisok.end();
+    iter2--;
+    while (iter1 != iter2) {
+        swap(*iter1, *iter2);
+        iter2--;
+        if (iter1 != iter2)
+            iter1++;
+    }
+    iter1 = spisok.begin();
+    while (iter1!=spisok.end())
+        cout << *iter1++ << " ";
+    _getch();
+}
